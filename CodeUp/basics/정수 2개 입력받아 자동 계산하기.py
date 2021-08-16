e5aa5cd175b10bb71024351d79a5077b8c157504
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-08-16 20:38:40 +0900

    [기초-산술연산] 6044

---
+++ CodeUp/basics/정수 2개 입력받아 자동 계산하기.py
@@ -0,0 +1,9 @@
+a, b = input().split()
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) // int(b))
+print(int(a) % int(b))
+print(format(float(a)/float(b), ".2f"))
+
