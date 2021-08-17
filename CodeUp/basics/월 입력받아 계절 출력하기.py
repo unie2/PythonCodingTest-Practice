807cfedae4f47ad37658fdf2407ec5e0326b833d
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-08-17 14:19:27 +0900

    [기초-조건/선택실행구조] 6070

---
+++ CodeUp/basics/월 입력받아 계절 출력하기.py
@@ -0,0 +1,10 @@
+input = int(input())
+
+if input == 12 or input == 1 or input == 2 :
+  print("winter")
+elif input//9 == 1 :
+  print("fall")
+elif input//6 == 1 :
+  print("summer")
+elif input//3 == 1 :
+  print("spring")
