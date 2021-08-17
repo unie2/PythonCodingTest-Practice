2524bdd9ca5ec8ba5f8e635ec0ed66870b000585
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-08-17 14:19:05 +0900

    [기초-조건/선택실행구조] 6069

---
+++ CodeUp/basics/평가 입력받아 다르게 출력하기.py
@@ -0,0 +1,12 @@
+input = input()
+
+if input == 'A' :
+  print("best!!!")
+elif input == 'B' :
+  print("good!!")
+elif input == 'C' :
+  print("run!")
+elif input == 'D' :
+  print("slowly~")
+else :
+  print("what?")
