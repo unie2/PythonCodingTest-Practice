669c99e162555b953a11258c4fa66153de5b14f3
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-09-17 18:04:57 +0900

    Create 소수(Prime Number).py

---
+++ 이코테/스터디/소수(Prime Number).py
@@ -0,0 +1,22 @@
+''' 기본적인 소수 판별 예제 '''
+def is_prime_number(x) :
+  for i in range(2, x) :
+    if x % i == 0 :
+      return False
+  return True
+
+print(is_prime_number(4))
+print(is_prime_number(7))
+
+
+''' 개선된 소수 판별 예제 '''
+import math
+
+def is_prime_number(x) :
+  for i in range(2, int(math.sqrt(x)) + 1) :
+    if x % i == 0 :
+      return False
+  return True
+
+print(is_prime_number(4))
+print(is_prime_number(7))
