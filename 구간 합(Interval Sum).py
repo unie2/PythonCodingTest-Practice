d479521c9fad453316c613cdf62cdb5026a29b62
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-09-27 10:55:47 +0900

    Interval Sum Example

---
+++ 구간 합(Interval Sum).py
@@ -0,0 +1,15 @@
+#데이터의 개수 N과 데이터 입력
+n = 5
+data = [10, 20, 30, 40, 50]
+
+# 접두사 합 배열 계산
+sum_value = 0
+prefix_sum = [0]
+for i in data :
+  sum_value += i
+  prefix_sum.append(sum_value)
+
+# 구간 합 계산(세번째~네번째)
+left = 3
+right = 4
+print(prefix_sum[right] - prefix_sum[left - 1])
