2e8d1fc56ab5efb59a50a7b9d784cb8e6c58a720
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-10-07 09:57:03 +0900

    [수학, 정수론, 소수 판정, 에라토스테네스의 체] 1929

---
+++ BOJ/소수 구하기.py
@@ -0,0 +1,14 @@
+m, n = map(int, input().split())
+
+def prime_number(x) :
+  if x == 1 :
+    return False
+  else :
+    for i in range(2, int(x**0.5) + 1) :
+      if x % i == 0 :
+        return False
+    return True
+
+for i in range(m, n + 1) :
+  if prime_number(i) :
+    print(i)
