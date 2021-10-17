c10f25326cb3b153010c6bc79fb7e5a9fa12618e
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-10-17 10:11:51 +0900

    [수학, 사칙연산] 5063

---
+++ BOJ/TGN.py
@@ -0,0 +1,11 @@
+tc = int(input())
+
+for _ in range(tc) :
+  r, e, c = map(int, input().split())
+
+  if e - c > r :
+    print("advertise")
+  elif e - c == r :
+    print("does not matter")
+  else :
+    print("do not advertise")
