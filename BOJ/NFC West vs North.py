72a89a83498b5a8ade9b0298e12684c05b8eee04
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-11-18 09:55:26 +0900

    [구현] 10170

---
+++ BOJ/NFC West vs North.py
@@ -0,0 +1,16 @@
+print("NFC West       W   L  T")
+print("-----------------------")
+print("Seattle        13  3  0")
+print("San Francisco  12  4  0")
+print("Arizona        10  6  0")
+print("St. Louis      7   9  0")
+print()
+print("NFC North      W   L  T")
+print("-----------------------")
+print("Green Bay      8   7  1")
+print("Chicago        8   8  0")
+print("Detroit        7   9  0")
+print("Minnesota      5  10  1")
+
+
+# 1. 입력받는 값은 없으므로 예제와 같이 NFC 서부와 북부 디비전 순위를 출력한다.
