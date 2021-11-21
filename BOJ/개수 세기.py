1f8d1dbb504729d96327fe57cef245336a25c2f0
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-11-21 14:25:45 +0900

    [구현] 10807

---
+++ BOJ/개수 세기.py
@@ -0,0 +1,9 @@
+n = int(input())
+data = list(map(int, input().split()))
+
+v = int(input())
+print(data.count(v))
+
+
+# 1. n개의 값을 입력받아 리스트 형태로 구성한다.
+# 2. 찾으려고 하는 정수(v)를 입력받아 count( )를 통해 data 리스트에 v가 몇개 있는지 출력한다.
