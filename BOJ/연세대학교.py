e381986b6045606588594b8df2e0f48c6a961e17
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2021-11-23 11:15:29 +0900

    [구현] 15680

---
+++ BOJ/연세대학교.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if n == 0 :
+  print('YONSEI')
+else :
+  print('Leading the Way to the Future')
+  
+  
+# 1. 입력받은 n이 0일 경우 연세대학교의 영문명을 출력한다.
+# 2. 입력받은 n이 1일 경우 연세대학교의 슬로건을 출력한다.
