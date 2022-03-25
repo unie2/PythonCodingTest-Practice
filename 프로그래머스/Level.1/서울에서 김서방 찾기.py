f9b9161a3f62a252a9df751632e76644313397d0
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-03-25 10:04:24 +0900

    [Level.1] 12919

---
+++ 프로그래머스/Level.1/서울에서 김서방 찾기.py
@@ -0,0 +1,8 @@
+def solution(seoul):
+    index_value = seoul.index("Kim")
+    
+    answer = "김서방은 %d에 있다" % index_value
+    return answer
+  
+  
+# 1. 리스트.index(찾을 요소) 를 사용하면 반복문을 작성하지 않고도 찾고자 하는 요소의 위치를 구할 수 있다.
