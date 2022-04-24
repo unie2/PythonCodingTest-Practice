e67df75755059d91b2ca565a0994750792e4eb34
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-04-25 08:44:36 +0900

    [D1] 2019

---
+++ SWEA/D1/더블더블.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+value = 1
+for _ in range(n + 1) :
+    print(value, end=" ")
+    value *= 2
+    
+'''
+1. value를 1로 초기화한 후 주어진 횟수만큼 반복문을 수행한다.
+2. 반복문 내부에서는 value값을 먼저 출력한 후 2를 곱한 값으로 갱신한다.
+
+'''
