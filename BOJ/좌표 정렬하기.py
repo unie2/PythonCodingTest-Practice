878ff2d7e9a288581cf21d51d84796ad6c88bc68
Yoonhee Lee <54324782+unie2@users.noreply.github.com>
2022-07-14 08:28:04 +0900

    [정렬] 11650

---
+++ BOJ/좌표 정렬하기.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+data = []
+for _ in range(n) :
+    x, y = map(int, input().split())
+    data.append((x, y))
+
+data = sorted(data, key=lambda x: (x[0], x[1]))
+
+for d in data :
+    print(d[0], d[1])
+    
+'''
+1. n개의 (x, y) 좌표를 입력받아 data 리스트에 저장한다.
+2. (x 좌표가 증가하는 순) -> (y 좌표가 증가하는 순) 으로 data 리스트 내 요소를 정렬한다.
+3. 정렬된 data 리스트의 각 x, y 좌표를 출력한다.
+
+'''
